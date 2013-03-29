f1f278f39d593ffba10333ad12ffd4d7119b818f
TheMilkmang <themilkman@epicmilk.com>
2013-03-28 23:22:18 -0700

    multiple contacts

--- AutoAnswer.py
+++ AutoAnswer.py
@@ -1,41 +1,47 @@
 import sys
 sys.path.append('./modules')
+
 import time
 import Skype4Py
 
+#Will segfault if Transoprt='x11' isn't there
 skype = Skype4Py.Skype(Transport='x11')
-CmdLine = ""
+#global cmdLine variable. So far is only used for adding people to the auto-answer list
+cmdLine = ""
+
 def init():
-	global CmdLine
-	try:
-	    CmdLine = sys.argv[1]
-	except:
-	    print 'Usage: python skypecall.py [user to auto-answer]'
-	    sys.exit()
+	global cmdLine
+	cmdLine = sys.argv
+	print len(cmdLine)
 	
-	
-	
-def CallStatusText(status):
+	#if no contact is specified
+	if len(cmdLine) < 2:
+		print 'Usage: python AutoAnswer.py [user] [user2] [user3] etc...'
+		sys.exit()
+#converts skype call status to readable text
+def callStatusText(status):
 	return skype.Convert.CallStatusToText(status)
 	
 
-
-def OnCall(call, status):
-	global CallStatus
-	global CmdLine
-	CallStatus = status
-	print 'Call status: ' + CallStatusText(status)
-	if CallStatusText(status) == "Calling":
-		if call.PartnerHandle == CmdLine:
+#Runs when call status changes
+def onCall(call, status):
+	global callStatus
+	global cmdLine
+	callStatus = status
+	print 'Call status: ' + callStatusText(status)
+	
+	if callStatusText(status) == "Calling":
+		if call.PartnerHandle in cmdLine:
 			call.Answer()
-		
+		else:
+			print 'no auto answer for', call.PartnerHandle
 
 def main():
 	global skype
 	init()
 	# Connect the Skype object to the Skype client.
 	skype.Attach()
-	skype.OnCallStatus = OnCall	
+	skype.OnCallStatus = onCall	
 
 	while not 2+2==6:
 		time.sleep(1)
