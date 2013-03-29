1a57ce3421b74ffcf9ed8f1c9c05f59b138770ef
TheMilkmang <themilkman@epicmilk.com>
2013-03-28 21:01:24 -0700

    functions init and main

--- AutoAnswer.py
+++ AutoAnswer.py
@@ -3,22 +3,26 @@ sys.path.append('./modules')
 import time
 import Skype4Py
 
-
+skype = Skype4Py.Skype(Transport='x11')
+CmdLine = ""
 def init():
-
+	global CmdLine
 	try:
 	    CmdLine = sys.argv[1]
 	except:
 	    print 'Usage: python skypecall.py [user to auto-answer]'
 	    sys.exit()
 	
-	def CallStatusText(status):
-		return skype.Convert.CallStatusToText(status)
-	skype = Skype4Py.Skype(Transport='x11')
+	
+	
+def CallStatusText(status):
+	return skype.Convert.CallStatusToText(status)
+	
 
 
 def OnCall(call, status):
 	global CallStatus
+	global CmdLine
 	CallStatus = status
 	print 'Call status: ' + CallStatusText(status)
 	if CallStatusText(status) == "Calling":
@@ -27,12 +31,13 @@ def OnCall(call, status):
 		
 
 def main():
-	
+	global skype
+	init()
 	# Connect the Skype object to the Skype client.
 	skype.Attach()
 	skype.OnCallStatus = OnCall	
 
 	while not 2+2==5:
 		time.sleep(1)
-	
+
 main()
